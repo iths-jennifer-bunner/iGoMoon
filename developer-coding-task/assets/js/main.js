9fabeda08af29998135f59adb13fbdc5e2a5f8cf
iths-jennifer-bunner <jenniferbunner@hotmail.com>
2021-08-15 17:16:41 +0200

    added navbar

--- developer-coding-task/assets/js/main.js
+++ developer-coding-task/assets/js/main.js
@@ -1,3 +1,9 @@
-(function ($) { 
-	
+(function($) {
+  $(document).ready(function() {
+    $("input:first-child").focus(function() {
+      $("#show")
+        .removeClass("hide")
+        .addClass("fadeIn");
+    });
+  });
 })(jQuery);
